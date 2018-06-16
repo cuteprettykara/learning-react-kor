05d485973ccb6e7a5d9b36141ec1632c49e13de5
HP-8570P\prettykara <cuteprettykara@gmail.com>
2018-06-16 11:05:30 +0900

    홈페이지에 Link 걸기

--- chapter-11/company-website-test/src/pages.js
+++ chapter-11/company-website-test/src/pages.js
@@ -4,9 +4,15 @@ import './stylesheets/pages.scss'
 export const Home = () => 
     <section className="home">
         <h1>[홈페이지]</h1>
+        <nav>
+            <Link to="about">[회사소개]</Link>
+            <Link to="events">[이벤트]</Link>
+            <Link to="products">[제품]</Link>
+            <Link to="contact">[고객지원]</Link>
+        </nav>
     </section>
 
-export const About = () => 
+export const About = () =>
     <section className="about">
         <h1>[회사소개]</h1>
     </section>
